ba5d23dbd82b8222a463338df8891d8b4860d65a
Tyler Campbell <tylercampbell@users.noreply.github.com>
2022-05-12 02:41:11 -0400

    basic clipboard tooltip

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,6 +1,12 @@
 var clipboard = new ClipboardJS('.clipboard');
+const tooltip = document.querySelector(".clipboard .tooltip")
 
 clipboard.on('success', function (e) {
+  tooltip.style.visibility = "visible";
+  setTimeout(function() {
+    tooltip.style.visibility = "hidden";
+  }, 1000);
+
   console.info('Action:', e.action);
   console.info('Text:', e.text);
   console.info('Trigger:', e.trigger);
